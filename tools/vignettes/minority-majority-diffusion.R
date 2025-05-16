60495b497369d9f0adf3a7ded4109cae08fca495
Matt Turner <maturner01@gmail.com>
2025-05-16 07:10:00 -0700

    symmetric homophily experiment working

--- tools/vignettes/minority-majority-diffusion.R
+++ tools/vignettes/minority-majority-diffusion.R
@@ -22,7 +22,7 @@ gen_homophily_abm <- function(param_row) {
   # Calculate number of minorty and majority agents
   N_min <- round(N * m)
   N_maj <- N - N_min
-  print(h)
+  
   # Initialize agent-based model with the homophily graph, ensuring connected graph
   h_graph <- make_homophily_network(
     group_sizes = c(N_min, N_maj),
@@ -32,7 +32,7 @@ gen_homophily_abm <- function(param_row) {
   # The network construction is random, doesn't always create connected network;
   # re-create h_graph until it's connected
   while (!igraph::is_connected(h_graph)) {
-    print("Not yet connected, attempting to create connected homophily network")
+    warning("Not yet connected, will retry to get connected homophily network")
     
     # Create the homophily network given the parameters
     h_graph <- make_homophily_network(
@@ -88,25 +88,37 @@ trial <- run_trial(abm)
 plot_prevalence(trial, tracked_behaviors = c("Adaptive"))
 
 #------------- CPU EXPERIMENT OVER A FEW HOMOPHILY VALUES --------------
-
+# devtools::load_all()
 n_agents = 100
-minority_fraction = 0.1
-mean_degree = 10
-homophily_vals = c(-0.5, -0.25, 0.0, 0.25, 0.5)
+minority_fraction = 0.2
+mean_degree = 15
+homophily_vals = c(-0.9, -0.75, -0.5, -0.25, 0.0, 0.25, 0.5, 0.75, 0.9)
 adaptive_fitness = 1.2
 start_group = "Both"
-# devtools::load_all()
+
+# We can run n_trials_per_param simulations for each combination of parameter
+# settings given in the free named parameters represented by ... in fn signature
 trials <- run_trials(
-  gen_homophily_abm, stop = socmod::fixated, 
-  n_trials_per_param = 10, .progress = TRUE, 
-  syncfile = "small-homoph.RData", 
-  # overwrite = T, 
+  
+  # Provide the agent-based model generator function first
+  gen_homophily_abm, 
+  
+  # Model specification parameters
   n_agents = n_agents, 
+  homophily = homophily_vals,
   minority_fraction = minority_fraction, 
   mean_degree = mean_degree,
   adaptive_fitness = adaptive_fitness, 
-  start_group = start_group, 
-  homophily = homophily_vals
+  start_group = start_group,
+  
+  # Trial parameter settings: how many trials per param and when to stop sims
+  n_trials_per_param = 20,
+  stop = socmod::fixated, 
+  
+  # System configuration: show progress and optionally sync/overwrite to file
+  .progress = TRUE, 
+  syncfile = "small-homoph.RData", 
+  overwrite = TRUE
 )
 
 prev_summary <- 
@@ -114,9 +126,9 @@ prev_summary <-
                        input_parameters = "homophily", 
                        across_trials = F) %>%
   dplyr::rename(Homophily = homophily) %>%
-  dplyr::mutate(Homophily = factor(homophily, homophily_vals))
+  dplyr::mutate(Homophily = factor(Homophily, homophily_vals))
 
-prev_summary %>% dplyr::filter(Homophily %in% c(-0.5, 0.0, 0.5)) %>%
+prev_summary %>% dplyr::filter(Homophily %in% c(-0.9, 0.0, 0.5, 0.9)) %>%
   ggplot(
     aes(x = Step, y = Prevalence, 
         color = Homophily, linetype = Homophily, group = trial_id)
@@ -124,3 +136,7 @@ prev_summary %>% dplyr::filter(Homophily %in% c(-0.5, 0.0, 0.5)) %>%
   geom_line(linewidth = 1.4, alpha = 0.875) + 
   theme_classic(base_size = 16) +
   scale_color_manual(values = unname(SOCMOD_PALETTE))
+
+
+outcomes_summary <- summarise_outcomes(trials, input_parameters = "homophily",
+                                       outcome_measures = c(""))
